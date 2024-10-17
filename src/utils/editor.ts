1f8143acc9526819dc508fafa202a9e4bd289636
Mohamed Slimani <slimani.m1993@gmail.com>
2024-10-18 00:18:27 +0100

    Refactor openEditor function to use promisifiedSpawn and add helper function for spawning processes

--- src/utils/editor.ts
+++ src/utils/editor.ts
@@ -4,6 +4,7 @@ import chalk from "chalk";
 import path from "path";
 import simpleGit, {SimpleGit} from "simple-git";
 import {promisify} from "util";
+import {SpawnOptions} from "node:child_process";
 
 const execAsync = promisify(spawn);
 const git: SimpleGit = simpleGit();
@@ -21,16 +22,8 @@ export async function openEditor(content: string): Promise<string> {
     await fs.writeFile(tempFilePath, content);
 
     try {
-      const process = spawn(editor.value, [tempFilePath], {stdio: 'inherit'});
 
-      // Optionally handle errors
-      process.on('error', (error) => {
-        console.error(chalk.red('Failed to start editor:', error));
-      });
-
-      while (process.connected) {
-        await new Promise(resolve => setTimeout(resolve, 100));
-      }
+      await promisifiedSpawn(editor.value, [tempFilePath], {stdio: 'inherit'})
 
       console.log(chalk.green('Editor closed.'));
       // Read the message back from the temporary file
@@ -38,11 +31,27 @@ export async function openEditor(content: string): Promise<string> {
       // Delete the temporary file after reading
       await fs.unlink(tempFilePath);
       return finalMessage.trim();
-
-
     } catch (error: any) {
       console.error(chalk.red('Error opening editor:', error.message));
       return ''; // Return an empty string if there's an error
     }
   }
+}
+
+function promisifiedSpawn(command: string, args: string[], options: SpawnOptions) {
+  return new Promise((resolve, reject) => {
+    const child = spawn(command, args, options);
+
+    child.on('exit', (code) => {
+      if (code === 0) {
+        resolve(code);
+      } else {
+        reject(new Error(`Process exited with code: ${code}`));
+      }
+    });
+
+    child.on('error', (error) => {
+      reject(error);
+    });
+  });
 }
